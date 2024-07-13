700720f85b5dffe0c6aabaf93c59e81b81cc463f
rishabh010105 <rishabh.delhi.2005@gmail.com>
2024-07-13 21:06:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+signInBtn.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpBtn.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+document.getElementById('form2').addEventListener('submit', function(event) {
+	event.preventDefault(); // Prevent the default form submission
+	window.location.href = 'hello.html'; // Redirect to hello.html
+});
